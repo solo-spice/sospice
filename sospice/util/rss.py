1492617d9796d93111b098016d71204b529a1332
Eric Buchlin <eric.buchlin@ias.u-psud.fr>
2023-05-13 09:21:56 +0200

    Add function for RSS

---
+++ sospice/util/rss.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def rss(a, axis=None):
+    """
+    Root sum square of array elements
+
+    Parameters
+    ----------
+    a: numpy.array
+        Input values
+    axis: None or int or tuple of ints
+        Axis or axes along which the root-sum-square is performed.
+    Return
+    ------
+    float
+        Root sum square of input values over a given axis
+    """
+    return np.linalg.norm(a, axis=axis)
