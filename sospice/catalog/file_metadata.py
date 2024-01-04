b4f641a432af5f54a3fe30c6c65ab37592d73b8c
Eric Buchlin <eric.buchlin@ias.u-psud.fr>
2024-01-04 22:56:13 +0100

    Plot FOV for a SPICE observation

--- sospice/catalog/file_metadata.py
+++ sospice/catalog/file_metadata.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass
 from pathlib import Path
+import warnings
 
+import numpy as np
 import pandas as pd
 import portion
 
-from astropy.utils.data import download_file
 from parfive import Downloader
+from astropy.utils.data import download_file
 import astropy.units as u
+from astropy.wcs import WCS, FITSFixedWarning
+from astropy.coordinates import SkyCoord
+from sunpy.coordinates import HeliographicStonyhurst, Helioprojective
+
+from sunpy.coordinates.utils import GreatArc  # check whether still needed
 
 from .release import Release
 
@@ -238,3 +245,171 @@ class FileMetadata:
         for r in ranges:
             intervals |= portion.closed(*r)
         return intervals
+
+    def get_pc_2d(self):
+        """
+        Get 2D PC matrix for observation, for x and y axes
+
+        Return
+        ------
+        dict
+            2D PC matrix element values
+
+        Note: a 4D PC matrix (with wavelength and time axes) is included
+        in the SPICE FITS files, but the full information cannot be retrieved
+        from the catalog.
+        """
+        cdelt_ratio = self.metadata.CDELT2 / self.metadata.CDELT1
+        crota = self.metadata.CROTA * u.deg
+        pc = dict()
+        pc["PC1_1"] = np.cos(crota).value
+        pc["PC1_2"] = -np.sin(crota).value * cdelt_ratio
+        pc["PC2_1"] = np.sin(crota).value / cdelt_ratio
+        pc["PC2_2"] = np.cos(crota).value
+        return pc
+
+    def get_crpix_2d(self):
+        """
+        Get 2D CRPIX values, for x and y axes (assuming FITS pixel indices convention)
+
+        Return
+        ------
+        dict:
+            CRPIX values
+        """
+        crpix = dict()
+        crpix["CRPIX1"] = self.metadata.NAXIS1 / 2 + 0.5
+        crpix["CRPIX2"] = self.metadata.NAXIS2 / 2 + 0.5
+        return crpix
+
+    def get_wcs_2d(self):
+        """
+        Get World Coordinates System object for the observation, for x and y axes
+
+        Return
+        ------
+        astropy.wcs.WCS
+            WCS object for observation
+
+        Note: more dimensions can be retrieved from the SPICE FITS files,
+        but not from the catalog.
+        """
+        useful_columns = [
+            "CDELT1",
+            "CDELT2",
+            "CRVAL1",
+            "CRVAL2",
+            "CTYPE1",
+            "CTYPE2",
+            "CUNIT1",
+            "CUNIT2",
+            "NAXIS1",
+            "NAXIS2",
+        ]
+        header_dict = self.metadata[useful_columns].to_dict()
+        header_dict.update(self.get_pc_2d())
+        header_dict.update(self.get_crpix_2d())
+        with warnings.catch_warnings():
+            # Ignore a warning on CROTA
+            # There should be an additional condition
+            # message="keyword looks very much like CROTAn but isn't.",
+            # but this does not work
+            warnings.filterwarnings("ignore", category=FITSFixedWarning)
+            wcs = WCS(header_dict)
+        return wcs
+
+    def get_observer(self):
+        """
+        Get observer position and time for the observation
+
+        Return
+        ------
+        atropy.coordinates.SkyCoord
+            Observer coordinates (defined in Stonyhurst frame)
+        """
+        # estimate DATE-AVG from available metadata
+        date_avg = self.metadata["DATE-BEG"] + pd.Timedelta(
+            self.metadata["TELAPSE"] / 2, "seconds"
+        )
+        observer = HeliographicStonyhurst(
+            self.metadata.HGLN_OBS * u.deg,
+            self.metadata.HGLT_OBS * u.deg,
+            self.metadata.DSUN_AU * u.au,
+            obstime=date_avg,
+        )
+        return observer
+
+    def get_fov(self, points=None, method=None):
+        """
+        Get the FOV coordinates (bottom left and top right vertices of rectangle
+        in helioprojective coordinates) for an observation
+
+        Parameters
+        ----------
+        points: int
+            Number of points on each FOV boundary edge
+        method: str
+            Interpolation method for each edge (if points > 2).
+            'interp': interpolate the edge segment in helioprojective coordinates;
+            'arc': consider the edge as a great arc.
+        """
+        if points is None:
+            points = 10
+        if method is None:
+            method = "interp"
+        wcs = self.get_wcs_2d()
+        # Take pixel size into account to get coordinates at the external edges of vertex pixels
+        min1 = -0.5
+        min2 = -0.5
+        max1 = self.metadata.NAXIS1 - 0.5
+        max2 = self.metadata.NAXIS2 - 0.5
+        vertex_pixels = [
+            [min1, min2],
+            [max1, min2],
+            [max1, max2],
+            [min1, max2],
+            [min1, min2],  # close to first vertex
+        ]
+        if (points > 2) and (method == "interp"):
+            vertex_all_pixels = list()
+            for i in range(4):
+                vertex1 = vertex_pixels[i]
+                vertex2 = vertex_pixels[i + 1]
+                coords = [
+                    np.linspace(vertex1[j], vertex2[j], num=points) for j in [0, 1]
+                ]
+                for k in range(points):
+                    vertex_all_pixels.append([coords[j][k] for j in [0, 1]])
+            vertex_pixels = vertex_all_pixels
+        vertices_world = np.array(wcs.pixel_to_world_values(vertex_pixels))
+        observer = self.get_observer()
+        frame = Helioprojective(observer=observer, obstime=observer.obstime)
+        fov_coords = SkyCoord(vertices_world * u.deg, frame=frame)
+        if (points > 2) and (method == "arc"):
+            fov_all_coords = list()
+            for i in range(4):
+                arc = GreatArc(fov_coords[i], fov_coords[i + 1], points=points)
+                # This is at the Sun distance of SO!
+                # and still in helioprojective coordinates (not clear that this is on the correct sphere)
+                fov_all_coords.append(arc.coordinates())
+            fov_coords = SkyCoord(fov_all_coords)
+        return fov_coords
+
+    def plot_fov(self, ax, **kwargs):
+        """
+        Plot SPICE FOV on a background map
+
+        Parameters
+        ----------
+        ax: matplotlib.axes.Axes
+            Axes (with relevant projection)
+        kwargs: dict
+            Keyword arguments.
+            'points' and 'method' are passed to FileMetadata.get_fov(),
+            other arguments are passed to ax.plot_coords()
+        """
+        fov_args = dict()
+        for k in ["points", "method"]:
+            fov_args[k] = kwargs.pop(k, None)
+        fov_coords = self.get_fov(**fov_args)
+        ax.plot_coord(fov_coords, **kwargs)
